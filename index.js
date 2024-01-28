b02bd34a5751f97f560f80960ebf039b76c844ad
Vipul Guru <125081495+VG3627@users.noreply.github.com>
2024-01-29 00:58:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cookieParser()) 
 app.use(express.static('public'))
-
+app.set('views', './views');
 app.set('view engine','ejs')
 
 app.use(express.urlencoded({extended:true})) ;
